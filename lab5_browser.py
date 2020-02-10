6f7dff69b4bdb34964b83cbc08f413d27ad959b8
Dhiraj <d.meenavilli@share.epsb.ca>
2020-02-09 20:16:55 -0700

    This is the final form of lab 5 with the updated stack module

--- lab5_browser.py
+++ lab5_browser.py
@@ -1,80 +1,115 @@
-#----------------------------------------------------
+# ----------------------------------------------------
 # Lab 5, Exercise 2: Web browser simulator
 # Purpose of program:
 #
-# Author: 
+# Author:
 # Collaborators/references:
-#----------------------------------------------------
+# ----------------------------------------------------
 
 from stack import Stack
 
 def getAction():
-    '''
-    Write docstring to describe function
-    Inputs: ?
-    Returns: ?
-    '''
-    #delete pass and write your code here
-    pass
+    """
+    This recives the action the user wishes to perform, and will wipe the forward stack
+    Inputs: None
+    Returns: User Action
+    :return:
+    """
+    userWant = input('Enter  = (to enter a new website address), <(back button), > (forward button), or q to quit the browser simulation ')
+    return userWant
+
 
 def goToNewSite(current, bck, fwd):
-    '''
-    Write docstring to describe function
-    Inputs: ?
-    Returns: ?
-    '''   
-    #delete pass and write your code here
-    pass
-    
+    """
+    This allows the user to view a new site
+    Inputs: The current webpages, the back stack, and the forward stack
+    Returns: the new website that is being looked at.
+    :param current:
+    :param bck:
+    :param fwd:
+    :return:
+    """
+    URL = input('What is the new website you would like to view. ')
+    fwd.clear()
+    fwd.push(URL)
+    bck.push(current)
+    return fwd.peek()
+
+
 def goBack(current, bck, fwd):
-    '''
-    Write docstring to describe function
-    Inputs: ?
-    Returns: ?
-    '''    
-    #delete pass and write your code here
-    pass
+    """
+    This allows the user to go back to a website they had previously been on.
+    Inputs: The current webpage, the back stack, and the forward stack.
+    Returns: The website previous to the current one being viewed
+    :param current:
+    :param bck:
+    :param fwd:
+    :return:
+    """
+    try:
+        page = bck.peek()
+        bck.pop()
+        fwd.push(current)
+    except:
+        page = current
+    return page
 
 def goForward(current, bck, fwd):
-    '''
-    Write docstring to describe function
-    Inputs: ?
-    Returns: ?
-    '''    
-    #delete pass and write your code here
-    pass
+    """
+    This function allows the user to go forward to a previously seen website
+    Inputs: The current webpage being viewed, the back stack, and the forward stack
+    Returns: The website that is ahead in the users history
+    :param current:
+    :param bck:
+    :param fwd:
+    :return:
+    """
+    try:
+        page = fwd.peek()
+        fwd.pop()
+        bck.push(current)
+    except:
+        page = current
+    return page
+
 
 def main():
-    '''
+    """
     Controls main flow of web browser simulator
     Inputs: N/A
     Returns: None
-    '''    
+    :return:
+    """
     HOME = 'www.cs.ualberta.ca'
     back = Stack()
     forward = Stack()
-    
+
     current = HOME
     quit = False
-    
+
     while not quit:
         print('\nCurrently viewing', current)
         try:
             action = getAction()
-            
+            if action != 'q' and action != '>' and action != '<' and action != '=':
+                raise Exception("Invalid Entry.")
+
         except Exception as actionException:
             print(actionException.args[0])
-            
+
         else:
             if action == '=':
                 current = goToNewSite(current, back, forward)
-            #TO DO: add code for the other valid actions ('<', '>', 'q')
-            #HINT: LOOK AT LAB 4
-            
-            
-    print('Browser closing...goodbye.')    
+            if action == '<':
+                current = goBack(current,back,forward)
+            if action == '>':
+                current = goForward(current,back,forward)
+            if action == 'q':
+                quit = True
 
-        
+    print('Browser closing...goodbye.')
+
+
 if __name__ == "__main__":
     main()
-    
+
--- stack.py
+++ stack.py
@@ -1,41 +1,49 @@
-#----------------------------------------------------
-# Stack implementation #2 
+# ----------------------------------------------------
+# Stack implementation #2
 # (Top of stack corresponds to back of list)
-# 
+#
 # Author: CMPUT 175 team
 # Updated by:
-#----------------------------------------------------
+# ----------------------------------------------------
 
 class Stack:
     def __init__(self):
         self.items = []
-    
+
     def push(self, item):
         self.items.append(item)
-    
-    # MODIFY: RAISE AN EXCEPTION IF THIS METHOD IS INVOKED ON AN EMPTY STACK
-    def pop(self):       
-        return self.items.pop()
-    
-    # MODIFY: RAISE AN EXCEPTION IF THIS METHOD IS INVOKED ON AN EMPTY STACK
-    def peek(self):      
-        return self.items[len(self.items)-1] 
-    
+
+    def pop(self):
+        errorMessage = 'This array is empty nothing can be popped'
+        try:
+            return self.items.pop()
+        except IndexError:
+            raise Exception(errorMessage)
+
+    def peek(self):
+        errorMessage = 'This array is empty'
+        try:
+            peekPoint = self.items[len(self.items)-1]
+            return peekPoint
+        except IndexError:
+            raise Exception(errorMessage)
+
     def isEmpty(self):
         return self.items == []
-    
+
     def size(self):
         return len(self.items)
-    
+
     def show(self):
         print(self.items)
-    
+
     def __str__(self):
         stackAsString = ''
         for item in self.items:
             stackAsString += item + ' '
         return stackAsString
-    
+
     def clear(self):
-        #TO DO: complete method according to updated ADT
-        pass       
+        if self.size() > 0:
+            self.items = []
+
